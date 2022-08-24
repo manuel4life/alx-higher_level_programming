b5dfb9ad7d24b49d373449964bc5868783474797
manuel4life <allotey4eva@gmail.com>
2022-08-23 21:59:02 -0400

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for k in range(ord('a'), ord('{')):
-        if (k != 101 and k != 113):
-            print("{:c}".format(k), end='')
+    if (k != 101 and k != 113):
+        print("{:c}".format(k), end='')
