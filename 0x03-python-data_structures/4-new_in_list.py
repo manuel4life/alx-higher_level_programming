2605d4462e1d7f41e4e692a2322b885d51eab490
manuel4life <allotey4eva@gmail.com>
2022-08-28 20:07:45 -0400

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+def new_in_list(my_list, idx, element):
+
+    if (idx < 0) or (idx > (len(my_list)-1)):
+        return my_list
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
