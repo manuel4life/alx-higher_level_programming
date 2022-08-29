05a20d6ff0f9324985d6168355a66ec52aae53c2
manuel4life <allotey4eva@gmail.com>
2022-08-28 20:25:24 -0400

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,57 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+int check_size(listint_t *head);
+
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * is_palindrome - check if the linked list is a palindrome
+ * @head: list to be checked
+ *
+ * Return: 0 if it isnt and 1 if it is
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
+	listint_t *temp = *head;
+	int size = check_size(temp), i = 0;
+	int check_A[12700];
 
-	if (head == NULL || *head == NULL)
-		return (1);
-
-	temp = *head;
-	while (temp) /* get len of list */
+	if (check_A == NULL)
+		return (0);
+	if (size == 0)
 	{
-		temp = temp->next;
-		len++;
+		return (1);
 	}
-	return (0);
+	while (temp != NULL)
+	{
+		check_A[i] = temp->n;
+		temp = temp->next;
+		i++;
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (check_A[i] != check_A[(size - 1) - i])
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * check_size - return size of list
+ * @head: list to be checked
+ *
+ * Return: size of list
+ */
+
+int check_size(listint_t *head)
+{
+	int i = 0;
+
+	while (head != NULL)
+	{
+		i++;
+		head = head->next;
+	}
+	return (i);
 }
