77a5f3fd43722e2d737e4342cb9db4b44eda73a3
manuel4life <allotey4eva@gmail.com>
2022-09-14 20:09:19 -0400

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list:
             if index < x:
                 print('{}'.format(my_list[index]), end='')
-                idx += 1
+                index += 1
 
         print()
     except TypeError:
