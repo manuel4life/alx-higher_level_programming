e4341c74c6a4b6ce03636ab9b88048aab5fd7b97
manuel4life <allotey4eva@gmail.com>
2022-09-29 11:25:56 -0400

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''from_JSON_string module'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    function that returns an object (Python data structure)
+    represented by a JSON string
+    '''
+    return json.loads(my_str)
