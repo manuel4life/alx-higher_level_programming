05a29e7f63f1cf2faa4509ec79f666843d5af881
manuel4life <allotey4eva@gmail.com>
2022-10-02 07:51:50 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python3
-'''List addition script'''
-from sys import argv
-'''
-Write a script that adds all arguments to a Python list,
-and then save them to a file:
-    You must use your function save_to_json_file
-    from 5-save_to_json_file.py
-    You must use your function load_from_json_file
-    from 6-load_from_json_file.py
-    The list must be saved as a JSON representation
-    in a file named add_item.json
-    If the file doesn’t exist, it should be created
-    You don’t need to manage file permissions / exception.
-'''
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
 if __name__ == "__main__":
-    def create_file():
-        '''creates a json file and returns py_list'''
-        py_list = []
-        save_to_json_file(py_list, "add_item.json")
-        py_list = load_from_json_file("add_item.json")
-        return py_list
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        py_list = load_from_json_file("add_item.json")
-    except (FileNotFoundError):
-        py_list = create_file()
-
-    py_list = load_from_json_file("add_item.json")
-    for i in range(len(argv)):
-        if i != 0:
-            py_list.append(argv[i])
-    save_to_json_file(py_list, "add_item.json")
-    py_list = load_from_json_file("add_item.json")
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
