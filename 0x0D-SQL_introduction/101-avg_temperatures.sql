6c2f0c29d24f51d0a03605a61bae71bd84472040
manuel4life <allotey4eva@gmail.com>
2022-11-24 23:47:07 +0000

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- Displays the average temperature..
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- display's the temperature of the top 3 ceties during July and August.
+SELECT city, AVG(value) as avg_temp
+  FROM temperatures
+  WHERE month = 7 OR month = 8
+  GROUP BY city
+  ORDER BY avg_temp DESC
+  LIMIT 3;
