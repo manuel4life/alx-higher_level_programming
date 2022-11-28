656f7ad6bbdc8ef7a36c655236b952e742bda6ce
manuel4life <allotey4eva@gmail.com>
2022-11-28 22:48:54 +0000

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// A script that prints C is fun/Python is cool/JavaScript is amazing.
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
+
