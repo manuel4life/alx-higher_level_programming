74a4ff788bcbc1b364f3673a10b2bea75dfc19e8
manuel4life <allotey4eva@gmail.com>
2022-11-30 22:06:43 +0000

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const list = require('./100-data').list;
-
-let k = 0;
-const newList = list.map(x => x * k++);
+const list = require('./100-data.js').list;
+let i = 0;
+const newList = list.map(x => x * i++);
 console.log(list);
 console.log(newList);
