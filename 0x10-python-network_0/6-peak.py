eaeb2182f4f44731ff7a27ac041188dedc36b9c2
manuel4life <allotey4eva@gmail.com>
2022-12-21 10:17:00 +0000

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,20 +11,11 @@ def find_peak(list_of_integers):
     # Edge case: if the list has length 1, return the only element
     if len(list_of_integers) == 1:
         return list_of_integers[0]
-
-    # Find the middle index of the list
     mid = len(list_of_integers) // 2
 
-    # If the middle element is greater than its neighbors, it is a peak
     if (mid == 0 or list_of_integers[mid-1] <= list_of_integers[mid]) and (mid == len(list_of_integers)-1 or list_of_integers[mid+1] <= list_of_integers[mid]):
         return list_of_integers[mid]
-
-    # If the middle element is not a peak and is greater than its left neighbor,
-    # search for a peak in the right half of the list
     elif mid > 0 and list_of_integers[mid-1] > list_of_integers[mid]:
         return find_peak(list_of_integers[:mid])
-
-    # If the middle element is not a peak and is less than its left neighbor,
-    # search for a peak in the left half of the list
     else:
         return find_peak(list_of_integers[mid+1:])
