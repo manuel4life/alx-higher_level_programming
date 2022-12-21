b6f0b6fc766af5a87f5a5eceeae36ac9dbcd3d7e
manuel4life <allotey4eva@gmail.com>
2022-12-21 10:12:32 +0000

    6-peak.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 
-"""Module containing the find_peak function."""
+"""find_peak function."""
 
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers.
-    Args:
-        list_of_integers: A list of unsorted integers.
-    Returns:
-        The first peak found in the list, or None if no peak is found.
-    """
+    # Edge case: if the list is empty, return None
     if not list_of_integers:
         return None
 
+    # Edge case: if the list has length 1, return the only element
     if len(list_of_integers) == 1:
         return list_of_integers[0]
 
-    middle = len(list_of_integers) // 2
-    if (list_of_integers[middle] > list_of_integers[middle - 1] and
-            list_of_integers[middle] > list_of_integers[middle + 1]):
-        return list_of_integers[middle]
-    elif list_of_integers[middle] < list_of_integers[middle - 1]:
-        return find_peak(list_of_integers[:middle])
+    # Find the middle index of the list
+    mid = len(list_of_integers) // 2
+
+    # If the middle element is greater than its neighbors, it is a peak
+    if (mid == 0 or list_of_integers[mid-1] <= list_of_integers[mid]) and (mid == len(list_of_integers)-1 or list_of_integers[mid+1] <= list_of_integers[mid]):
+        return list_of_integers[mid]
+
+    # If the middle element is not a peak and is greater than its left neighbor,
+    # search for a peak in the right half of the list
+    elif mid > 0 and list_of_integers[mid-1] > list_of_integers[mid]:
+        return find_peak(list_of_integers[:mid])
+
+    # If the middle element is not a peak and is less than its left neighbor,
+    # search for a peak in the left half of the list
     else:
-        return find_peak(list_of_integers[middle + 1:])
+        return find_peak(list_of_integers[mid+1:])
