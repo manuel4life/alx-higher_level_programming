d13816189890831214cf30331c5ef9914b737f43
manuel4life <allotey4eva@gmail.com>
2022-12-29 11:49:55 +0000

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Function that posts data to a server
+'''
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+def post_data(url, email):
+    '''
+    To post data to a given url
+    '''
+    url = url
+    data = {'email': email}
+
+    data = urllib.parse.urlencode(data).encode('ascii')
+
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        email_info = response.read().decode('utf-8')
+    print(email_info)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    post_data(url, email)
