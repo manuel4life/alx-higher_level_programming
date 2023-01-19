a042f0fba4f0e643212bf050a80ba34fe94053cc
manuel4life <allotey4eva@gmail.com>
2023-01-19 22:33:44 +0000

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+'strict mode';
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (err, response) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
