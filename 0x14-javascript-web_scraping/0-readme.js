a22b8ba6ec409cec5e84c58adb230ce6fbb1e0cd
manuel4life <allotey4eva@gmail.com>
2023-01-19 22:30:14 +0000

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+'use strict';
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
