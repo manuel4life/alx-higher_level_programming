6d15b47e7ca8f5ae45da4b88aa2bd42b56e15eef
manuel4life <allotey4eva@gmail.com>
2023-01-19 22:38:58 +0000

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+'strict mode';
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(file, body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
